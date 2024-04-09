dfc4d55453a21ac12d8a2d9d7d5047252bf20e34
Georgios Vlassis <gvlassis@mailbox.org>
2024-04-09 12:50:44 +0200

    Added emojis

--- src/soft.sh
+++ src/soft.sh
@@ -1,10 +1,12 @@
 kernel_name="$(uname)"
 
 if [ "${kernel_name}" = "Darwin" ]; then
+	printf "🍎\n"
 	system_version="$(system_profiler SPSoftwareDataType | awk -F ': ' '/System Version/ {printf $2}')"
 	os_name=$(cut -d " " -f 1 <<< "$system_version")
 	os_version=$(cut -d " " -f 2 <<< "$system_version")
 elif [ "${kernel_name}" = "Linux" ]; then
+	printf "🐧\n"
 	os_name=$(awk -F '=' '/^NAME/ {printf $2}' /etc/*-release | tr -d '"')
 	os_version=$(awk -F '=' '/^VERSION_ID/ {printf $2}' /etc/*-release | tr -d '"')
 fi
